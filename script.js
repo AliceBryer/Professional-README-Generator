a177c06197a9f7e87180c08047e3016a976be1ee
Alice Bryer <alicebryer123@gmail.com>
2022-06-01 17:29:19 +0100

    First question tested

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 const fs = require('fs').promises;
-const inquirer = require("inquirer")
+const inquirer = require("inquirer");
+
+async function getUserInput () {
+
+    const answers = await inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            messge: "What is the name of your project?",
+        },
+    ]);
+}
+
+getUserInput();
